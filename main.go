faadf5a3cc0fc3f9c6d8aead71aea440078cd98e
Grad3030 <Grad3030@mail.ru>
2025-10-23 14:09:30 +0300

    hw2

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("привет мир")
+	var n, count int
+	fmt.Println("введите число")
+	fmt.Fscan(os.Stdin, &n)
+
+	for n < 12307 {
+		if n < 0 {
+			n = n * (-1)
+			count++
+		}
+		if n%7 == 0 {
+			n = n * 39
+			count++
+		}
+		if n%9 == 0 {
+			n = n*13 + 1
+			count++
+			continue
+		}
+		if count == 0 {
+			n = (n + 2) * 3
+		}
+	}
+	fmt.Println(n)
+
 }
